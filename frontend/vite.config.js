88d269242cb892f5466b2d5ead28d6ca56539fa3
rob-searles <rsearles@s4netquest.com>
2025-05-27 18:14:41 -0400

    Vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,20 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3001,
-    host: 'localhost',
-    open: true,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-        secure: false,
-      }
-    }
-  },
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets'
-  },
-  base: './'
+    port: 3001
+  }
 })
